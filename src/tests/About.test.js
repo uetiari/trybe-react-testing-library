4c72eece183ea71eb418e220dc4d1643f6af4066
uetiari <arianeueti@gmail.com>
2021-10-02 16:29:33 -0300

    Correção Lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './utils/renderWithRouter';
-import App from '../App';
+import About from '../components/About';
 
 describe('Testa o About.js', () => {
   test('se a página contém um heading h2 com o texto About Pokédex', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(<About />);
 
     const aboutTitle = screen.getByRole('heading', { name: 'About Pokédex' });
     expect(aboutTitle).toBeInTheDocument();
   });
 
   test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(<About />);
 
     const paragOne = screen.getByText(/This application simulates a Pokédex/i);
     expect(paragOne).toBeInTheDocument();
+    
     const paragTwo = screen.getByText(/One can filter Pokémons by type/i);
     expect(paragTwo).toBeInTheDocument();
-    
   });
 
   test('se a página contém a seguinte imagem de uma Pokédex', () => {
