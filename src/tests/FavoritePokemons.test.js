a898bc1ccb50f6767a779be237bf233de02c719f
uetiari <arianeueti@gmail.com>
2021-10-04 19:28:46 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testa o Favorite Pokémon.js', () => {
+  test('se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const favorite = screen.getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeInTheDocument();
+  });
+});
