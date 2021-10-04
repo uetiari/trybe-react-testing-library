b62827c0745fab50ed69088ed58c7c0979822723
uetiari <arianeueti@gmail.com>
2021-10-04 19:57:59 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Testa o NotFound.js', () => {
+  test('se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleNotFound = screen.getByRole('heading', { level: 2 });
+    expect(titleNotFound).toBeInTheDocument();
+
+    const titleContent = screen.getByText(/Page requested not found/i);
+    expect(titleContent).toBeInTheDocument();
+
+    const titleImage = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(titleImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
